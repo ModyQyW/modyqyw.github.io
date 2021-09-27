25f6641e00fedb6934586d190865ac1a2aa42d9b
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-09-27 14:03:50 +0800

    chore: update config

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,63 @@
+import { defineUserConfig } from 'vuepress';
+import type { DefaultThemeOptions } from 'vuepress';
+
+const mode = process.env.MODE || 'github';
+const hostname =
+  mode === 'github' ? 'https://modyqyw.top' : 'https://modyqyw.gitee.io';
+const repo =
+  mode === 'github'
+    ? 'https://github.com/ModyQyW/modyqyw.github.io'
+    : 'https://gitee.com/ModyQyW/ModyQyW';
+const repoLabel = mode === 'github' ? 'Github' : 'Gitee';
+
+export default defineUserConfig<DefaultThemeOptions>({
+  lang: 'zh-Hans',
+  title: "ModyQyW's Site",
+  description: '基于 vuepress 建设的个人站，如有问题请尝试强制刷新。',
+  head: [['link', { rel: 'manifest', href: '/manifest.webmanifest' }]],
+  themeConfig: {
+    navbar: [
+      { text: '首页', link: '/' },
+      { text: '学习路径', link: '/roadmap/' },
+      { text: '环境配置', link: '/environment/' },
+      { text: '开发', link: '/development/' },
+      { text: '安全', link: '/safety/' },
+      { text: 'Webpack', link: '/webpack/' },
+      { text: '关于', link: '/about/' },
+    ],
+    logo: '/images/w256.png',
+    repo,
+    repoLabel,
+    sidebar: 'auto',
+    sidebarDepth: 3,
+    docsBranch: 'origin',
+  },
+  markdown: {
+    toc: {
+      level: [2, 3, 4],
+    },
+    extractHeaders: {
+      level: [2, 3, 4],
+    },
+  },
+  plugins: [
+    ['@vuepress/plugin-search'],
+    ['@vuepress/pwa'],
+    ['@vuepress/plugin-pwa-popup'],
+    [
+      '@vuepress/plugin-shiki',
+      {
+        theme: 'github-dark',
+      },
+    ],
+    ['@vuepress/plugin-debug'],
+    [
+      '@snippetors/vuepress-plugin-code-copy',
+      {
+        backgroundTransition: false,
+      },
+    ],
+    ['vuepress-plugin-seo2'],
+    ['vuepress-plugin-sitemap2'],
+  ],
+});
