142c39bd65b6513fb211bb557c09c77181cd2368
ModyQyW <wurui7@mail3.sysu.edu.cn>
2022-02-04 23:54:19 +0800

    chore: update config

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -24,28 +24,22 @@ export default defineUserConfig<DefaultThemeOptions>({
     hostname,
     navbar: [
       {
-        text: '整理归纳',
+        text: '收集整理',
         children: [
-          {
-            text: '收集整理',
-            children: [
-              '/organize-and-summarize/conduct/',
-              '/organize-and-summarize/roadmap/',
-              '/organize-and-summarize/development/',
-            ],
-          },
-          {
-            text: '归纳输出',
-            children: [
-              '/organize-and-summarize/environment/',
-              '/organize-and-summarize/server/',
-              {
-                text: 'webpack',
-                link: '/organize-and-summarize/webpack/introduction.md',
-              },
-              '/organize-and-summarize/safety/',
-            ],
-          },
+          '/organize/what',
+          '/organize/conduct/',
+          '/organize/roadmap/',
+          { text: '开发相关', link: '/organize/development/01.introduction.md' },
+        ],
+      },
+      {
+        text: '归纳输出',
+        children: [
+          '/summarize/what',
+          '/summarize/environment/',
+          '/summarize/server/',
+          { text: 'webpack', link: '/summarize/webpack/01.introduction.md' },
+          '/summarize/safety/',
         ],
       },
       { text: 'leetcode', link: '/leetcode/0001.two-sum.md' },
@@ -55,93 +49,98 @@ export default defineUserConfig<DefaultThemeOptions>({
     repo,
     repoLabel,
     sidebar: {
-      '/organize-and-summarize/conduct/': [
+      '/organize/': [
         {
           text: '收集整理',
           children: [
-            '/organize-and-summarize/conduct/',
-            '/organize-and-summarize/roadmap/',
-            '/organize-and-summarize/development/',
+            '/organize/what',
+            '/organize/conduct/',
+            '/organize/roadmap/',
+            { text: '开发相关', link: '/organize/development/01.introduction.md' },
           ],
         },
       ],
-      '/organize-and-summarize/roadmap/': [
+      '/organize/development/': [
         {
           text: '收集整理',
           children: [
-            '/organize-and-summarize/conduct/',
-            '/organize-and-summarize/roadmap/',
-            '/organize-and-summarize/development/',
+            '/organize/what',
+            '/organize/conduct/',
+            '/organize/roadmap/',
+            {
+              text: '开发相关',
+              children: [
+                '/organize/development/01.introduction.md',
+                '/organize/development/02.technique.md',
+                '/organize/development/03.computer-basics.md',
+                '/organize/development/04.coding-fonts.md',
+                '/organize/development/05.frontend-basics.md',
+                '/organize/development/06.static-website-generation.md',
+                '/organize/development/07.ui-and-ux.md',
+                '/organize/development/08.specification.md',
+                '/organize/development/09.cdn.md',
+                '/organize/development/10.build.md',
+                '/organize/development/11.debug.md',
+                '/organize/development/12.test.md',
+                '/organize/development/13.release-and-deploy.md',
+                '/organize/development/14.ci-and-cd.md',
+                '/organize/development/15.monitor.md',
+                '/organize/development/16.devsecops.md',
+                '/organize/development/17.vanilla.md',
+                '/organize/development/18.react.md',
+                '/organize/development/19.vue.md',
+                '/organize/development/20.micro-frontend.md',
+                '/organize/development/21.virtual-dom.md',
+                '/organize/development/22.interview.md',
+                '/organize/development/23.cli.md',
+                '/organize/development/24.server.md',
+                '/organize/development/25.continuous-growth.md',
+                '/organize/development/26.we-media.md',
+                '/organize/development/27.open-source.md',
+                '/organize/development/28.independent-developer.md',
+                '/organize/development/29.reference.md',
+                '/organize/development/30.acknowledge.md',
+              ],
+            },
           ],
         },
       ],
-      '/organize-and-summarize/development/': [
-        {
-          text: '收集整理',
-          children: [
-            '/organize-and-summarize/conduct/',
-            '/organize-and-summarize/roadmap/',
-            '/organize-and-summarize/development/',
-          ],
-        },
-      ],
-      '/organize-and-summarize/environment/': [
+      '/summarize/': [
         {
           text: '归纳输出',
           children: [
-            '/organize-and-summarize/environment/',
-            '/organize-and-summarize/server/',
-            { text: 'webpack', link: '/organize-and-summarize/webpack/introduction.md' },
-            '/organize-and-summarize/safety/',
+            '/summarize/what',
+            '/summarize/environment/',
+            '/summarize/server/',
+            { text: 'webpack', link: '/summarize/webpack/01.introduction.md' },
+            '/summarize/safety/',
           ],
         },
       ],
-      '/organize-and-summarize/server/': [
+      '/summarize/webpack/': [
         {
           text: '归纳输出',
           children: [
-            '/organize-and-summarize/environment/',
-            '/organize-and-summarize/server/',
-            { text: 'webpack', link: '/organize-and-summarize/webpack/introduction.md' },
-            '/organize-and-summarize/safety/',
-          ],
-        },
-      ],
-      '/organize-and-summarize/webpack/': [
-        {
-          text: '归纳输出',
-          children: [
-            '/organize-and-summarize/environment/',
-            '/organize-and-summarize/server/',
+            '/summarize/environment/',
+            '/summarize/server/',
             {
               text: 'webpack',
               children: [
-                '/organize-and-summarize/webpack/introduction.md',
-                '/organize-and-summarize/webpack/why.md',
-                '/organize-and-summarize/webpack/what.md',
-                '/organize-and-summarize/webpack/basic-concepts.md',
-                '/organize-and-summarize/webpack/demo01.md',
-                '/organize-and-summarize/webpack/demo02.md',
-                '/organize-and-summarize/webpack/demo03.md',
-                '/organize-and-summarize/webpack/demo04.md',
-                '/organize-and-summarize/webpack/expansion.md',
-                '/organize-and-summarize/webpack/end.md',
-                '/organize-and-summarize/webpack/reference.md',
-                '/organize-and-summarize/webpack/acknowledgement.md',
+                '/summarize/webpack/01.introduction.md',
+                '/summarize/webpack/02.why.md',
+                '/summarize/webpack/03.what.md',
+                '/summarize/webpack/04.basic-concepts.md',
+                '/summarize/webpack/05.demo01.md',
+                '/summarize/webpack/06.demo02.md',
+                '/summarize/webpack/07.demo03.md',
+                '/summarize/webpack/08.demo04.md',
+                '/summarize/webpack/09.end.md',
+                '/summarize/webpack/10.expansion.md',
+                '/summarize/webpack/11.reference.md',
+                '/summarize/webpack/12.acknowledgement.md',
               ],
             },
-            '/organize-and-summarize/safety/',
-          ],
-        },
-      ],
-      '/organize-and-summarize/safety/': [
-        {
-          text: '归纳输出',
-          children: [
-            '/organize-and-summarize/environment/',
-            '/organize-and-summarize/server/',
-            { text: 'webpack', link: '/organize-and-summarize/webpack/introduction.md' },
-            '/organize-and-summarize/safety/',
+            '/summarize/safety/',
           ],
         },
       ],
