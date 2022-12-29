223746280e42e88d1d2a06a9bec8bfead0ff58cf
ModyQyW <wurui-dev@foxmail.com>
2022-12-29 21:54:01 +0800

    chore: update vitepress config

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -26,6 +26,7 @@ export default withMermaid(
         { text: '教程', link: '/tutorials/', activeMatch: '/tutorials/' },
         { text: '博客', link: '/blogs/', activeMatch: '/blogs/' },
         { text: '关于', link: '/about/' },
+        { text: '赞赏', link: 'https://github.com/ModyQyW/sponsors' },
       ],
       sidebar: {
         '/cheat-sheet': [],
@@ -43,9 +44,9 @@ export default withMermaid(
         copyright: 'Copyright © 2020-present ModyQyW',
       },
       algolia: {
-        appId: 'Z14RO4ODPO',
-        apiKey: 'bc7c3bfb65339c025cdced95c50cb051',
-        indexName: 'modyqyw',
+        appId: process.env.ALGOLIA_APP_ID!,
+        apiKey: process.env.ALGOLIA_API_KEY!,
+        indexName: process.env.ALGOLIA_INDEX_NAME!,
       },
     },
     transformHtml: (_, id, { pageData }) => {
