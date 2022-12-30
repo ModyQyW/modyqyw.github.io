464fc8fbacb28e25aea8edf8361425b25c4ac5a5
ModyQyW <wurui-dev@foxmail.com>
2022-12-30 21:42:57 +0800

    chore: remove mermaid

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,7 +2,6 @@ import { createWriteStream, readdirSync, readFileSync, statSync } from 'node:fs'
 import { resolve, join } from 'node:path';
 import { SitemapStream } from 'sitemap';
 import { defineConfig } from 'vitepress';
-import { withMermaid } from 'vitepress-plugin-mermaid';
 import type { DefaultTheme } from 'vitepress';
 
 const cwd = process.cwd();
@@ -53,81 +52,79 @@ const getBlogsNav = () => {
   } as DefaultTheme.NavItem;
 };
 
-export default withMermaid(
-  defineConfig({
-    // app configs
-    head: [
-      [
-        'script',
-        {},
-        `
+export default defineConfig({
+  // app configs
+  head: [
+    [
+      'script',
+      {},
+      `
         navigator.serviceWorker.getRegistrations().then((registrations) => {
           for (let r of registrations)
             r.unregister()
         })
         `,
-      ],
     ],
-    lang: 'zh-Hans',
-    lastUpdated: true,
-    markdown: {
-      lineNumbers: true,
-      theme: 'github-dark',
+  ],
+  lang: 'zh-Hans',
+  lastUpdated: true,
+  markdown: {
+    lineNumbers: true,
+    theme: 'github-dark',
+  },
+  title: "ModyQyW's Site",
+  // theme configs
+  themeConfig: {
+    nav: [
+      { text: '速查表', link: '/cheat-sheets/' },
+      { text: '教程', link: '/tutorials/' },
+      getBlogsNav(),
+      { text: '关于', link: '/about/' },
+      { text: '赞赏', link: 'https://github.com/ModyQyW/sponsors' },
+    ],
+    sidebar: {
+      '/cheat-sheet': [],
+      '/tutorials': [],
+      '/blogs': getBlogsSidebar(),
     },
-    title: "ModyQyW's Site",
-    // theme configs
-    themeConfig: {
-      nav: [
-        { text: '速查表', link: '/cheat-sheets/' },
-        { text: '教程', link: '/tutorials/' },
-        getBlogsNav(),
-        { text: '关于', link: '/about/' },
-        { text: '赞赏', link: 'https://github.com/ModyQyW/sponsors' },
-      ],
-      sidebar: {
-        '/cheat-sheet': [],
-        '/tutorials': [],
-        '/blogs': getBlogsSidebar(),
+    socialLinks: [
+      {
+        icon: 'github',
+        link: 'https://github.com/ModyQyW/modyqyw.github.io',
       },
-      socialLinks: [
-        {
-          icon: 'github',
-          link: 'https://github.com/ModyQyW/modyqyw.github.io',
-        },
-      ],
-      footer: {
-        message: 'Released under the MIT License.',
-        copyright: 'Copyright © 2020-present ModyQyW',
-      },
-      algolia: {
-        appId: process.env.ALGOLIA_APP_ID!,
-        apiKey: process.env.ALGOLIA_API_KEY!,
-        indexName: process.env.ALGOLIA_INDEX_NAME!,
-      },
+    ],
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2020-present ModyQyW',
     },
-    transformHtml: (_, id, { pageData }) => {
-      // clean urls mode
-      // if (!/[\\/]404\.html$/.test(id)) {
-      //   links.push({
-      //     url: pageData.relativePath.replace(/((^|\/)index)?\.md$/, '$2'),
-      //     lastmod: pageData.lastUpdated,
-      //   });
-      // }
-      // not clean urls mode
-      if (!/[\\/]404\.html$/.test(id)) {
-        links.push({
-          url: pageData.relativePath.replace(/\.md$/, '.html'),
-          lastmod: pageData.lastUpdated,
-        });
-      }
+    algolia: {
+      appId: process.env.ALGOLIA_APP_ID!,
+      apiKey: process.env.ALGOLIA_API_KEY!,
+      indexName: process.env.ALGOLIA_INDEX_NAME!,
     },
-    buildEnd: async ({ outDir }) => {
-      const sitemap = new SitemapStream({ hostname });
-      const writeStream = createWriteStream(resolve(outDir, 'sitemap.xml'));
-      sitemap.pipe(writeStream);
-      links.forEach((link) => sitemap.write(link));
-      sitemap.end();
-      await new Promise((r) => writeStream.on('finish', r));
-    },
-  }),
-);
+  },
+  transformHtml: (_, id, { pageData }) => {
+    // clean urls mode
+    // if (!/[\\/]404\.html$/.test(id)) {
+    //   links.push({
+    //     url: pageData.relativePath.replace(/((^|\/)index)?\.md$/, '$2'),
+    //     lastmod: pageData.lastUpdated,
+    //   });
+    // }
+    // not clean urls mode
+    if (!/[\\/]404\.html$/.test(id)) {
+      links.push({
+        url: pageData.relativePath.replace(/\.md$/, '.html'),
+        lastmod: pageData.lastUpdated,
+      });
+    }
+  },
+  buildEnd: async ({ outDir }) => {
+    const sitemap = new SitemapStream({ hostname });
+    const writeStream = createWriteStream(resolve(outDir, 'sitemap.xml'));
+    sitemap.pipe(writeStream);
+    links.forEach((link) => sitemap.write(link));
+    sitemap.end();
+    await new Promise((r) => writeStream.on('finish', r));
+  },
+});
