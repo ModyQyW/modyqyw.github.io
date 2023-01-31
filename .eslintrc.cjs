c1546bd34824369ca96673b32be7a082e051c051
ModyQyW <wurui-dev@foxmail.com>
2023-01-31 14:48:06 +0800

    chore: update config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,7 @@
 module.exports = {
-  root: true,
-  extends: [
-    require.resolve('@modyqyw/fabric/eslint/vanilla'),
-    require.resolve('@modyqyw/fabric/eslint/typescript'),
-    require.resolve('@modyqyw/fabric/eslint/vue'),
-  ],
+  extends: [require.resolve('@modyqyw/fabric/eslint')],
+  rules: {
+    'n/no-unpublished-import': 'off',
+    'n/no-unpublished-require': 'off',
+  },
 };
