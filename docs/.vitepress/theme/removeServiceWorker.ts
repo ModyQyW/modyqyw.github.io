12295bf6fc5e97de17f08b5185f7a5433925acd4
ModyQyW <wurui-dev@foxmail.com>
2023-02-02 15:00:25 +0800

    fix: detect node first

--- docs/.vitepress/theme/removeServiceWorker.ts
+++ docs/.vitepress/theme/removeServiceWorker.ts
@@ -1,6 +1,8 @@
+import isNode from 'detect-node';
+
 // eslint-disable-next-line unicorn/prefer-top-level-await
 (async () => {
-  console.log('in remove');
+  if (isNode) return;
   const registrations = await navigator.serviceWorker.getRegistrations();
   for (const registration of registrations) {
     if (
