48174c640ac5a0d3993bccc9cd249f54560a6062
ModyQyW <wurui-dev@foxmail.com>
2023-08-03 14:17:47 +0800

    chore: update script

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,7 +1,4 @@
 module.exports = {
   'package.json': 'sort-package-json',
   '*.md': 'markdownlint --fix --ignore-path=.gitignore',
-  '*.{css,scss,vue}': 'stylelint --fix --cache --ignore-path=.gitignore',
-  '*.{js,cjs,mjs,jsx,ts,cts,mts,tsx,vue}': 'eslint --fix --cache --ignore-path=.gitignore',
-  '*.{ts,cts,mts,tsx,vue}': () => 'vue-tsc -p tsconfig.json --noEmit'
 };
